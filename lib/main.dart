b6685669787927273f8a359058ee97923f5b5e4a
kevin <albinodemetrok@gmail.com>
2024-10-08 14:57:27 -0300

    ajustando layout dos itens da lista

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,7 @@ class MistakesTrackerApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return
-    MaterialApp(
+    return MaterialApp(
       home: MistakesTrackerPage(),
     );
   }
@@ -17,9 +16,9 @@ class MistakesTrackerApp extends StatelessWidget {
 class MistakesTrackerPage extends StatelessWidget {
   MistakesTrackerPage({super.key});
 
-  final List<Map<String, String>> lista = [
-    {'title' : 'teste'},
-    {'title' : 'teste2'}
+  final List<Map<String, Object>> lista = [
+    {'title': 'teste', 'qtd': 1},
+    {'title': 'teste2', 'qtd': 23}
   ];
 
   @override
@@ -32,16 +31,34 @@ class MistakesTrackerPage extends StatelessWidget {
           final itemList = lista[index];
           return Card(
             elevation: 5,
-            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-            child: ListTile(
-              title: Text(
-                itemList['title'].toString(),
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              trailing: IconButton(
-                onPressed: null,
-                icon: Icon(Icons.add),
-                color: Theme.of(context).colorScheme.error,
+            margin: const EdgeInsets.symmetric(vertical: 8),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 2),
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  child: Text('${itemList['qtd']}'),
+                ),
+                title: Text(
+                  itemList['title'].toString(),
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                trailing: FittedBox(
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: null,
+                        icon: const Icon(Icons.remove),
+                        color: Theme.of(context).colorScheme.error,
+                      ),
+                      IconButton(
+                        onPressed: null,
+                        icon: const Icon(Icons.add),
+                        color: Theme.of(context).colorScheme.error,
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           );
