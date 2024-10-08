e49892ffbdd14e1f715dc5ee850fc895c9112e38
kevin <albinodemetrok@gmail.com>
2024-10-08 19:35:13 -0300

    implementando banco e funções

---
+++ lib/database/database_helper.dart
@@ -0,0 +1,48 @@
+import 'package:mistakes_tracker/entity/Mistake.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+
+  factory DatabaseHelper() => _instance;
+
+  DatabaseHelper._internal();
+  Database? _database;
+
+  Future<Database> get database async {
+    _database ??= await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), 'mistakes_tracker.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE mistakes(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            quantity INTEGER NOT NULL, 
+            date TEXT NOT NULL
+          ) 
+        ''');
+      },
+    );
+  }
+
+  Future<void> insertMistake(Mistake mistake) async{
+    final db = await database;
+    await db.insert('mistakes', mistake.toMap(), conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  Future<List<Mistake>> getMistakes() async{
+    final db = await database;
+    final List<Map<String, dynamic>> mistakeMaps = await db.query('mistakes');
+    return List.generate(mistakeMaps.length, (i) {
+      return Mistake.fromMap(mistakeMaps[i]);
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mistakes_tracker/components/mistake_form.dart';
+import 'package:mistakes_tracker/database/database_helper.dart';
 import 'package:mistakes_tracker/entity/Mistake.dart';
 import 'package:mistakes_tracker/components/mistakes_list.dart';
 
@@ -24,15 +25,22 @@ class MistakesTrackerPage extends StatefulWidget {
 }
 
 class _MistakesTrackerPageState extends State<MistakesTrackerPage> {
-  final List<Mistake> mistakes = [
+  final DatabaseHelper _dbHelper = DatabaseHelper();
+  List<Mistake> _mistakes = [
     Mistake(title: 'title', quantity: 21, date: DateTime.now())
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMistakes();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: MistakesList(
-          mistakes: mistakes,
+          mistakes: _mistakes,
           onPlus: _increaseQuantity,
           onMinus: _decreaseQuantity),
       floatingActionButton: FloatingActionButton(
@@ -44,14 +52,14 @@ class _MistakesTrackerPageState extends State<MistakesTrackerPage> {
 
   _increaseQuantity(int quantity, int index) {
     setState(() {
-      mistakes[index].quantity++;
+      _mistakes[index].quantity++;
     });
   }
 
   _decreaseQuantity(int quantity, int index) {
     if (quantity > 0) {
       setState(() {
-        mistakes[index].quantity--;
+        _mistakes[index].quantity--;
       });
     }
   }
@@ -70,9 +78,19 @@ class _MistakesTrackerPageState extends State<MistakesTrackerPage> {
   _addMistake(String title) {
     Mistake mistake =
         new Mistake(title: title, quantity: 0, date: DateTime.now());
-    setState(() {
-      mistakes.add(mistake);
-    });
+    _insertMistake(mistake);
     Navigator.of(context).pop();
+  }
+
+  Future<void> _insertMistake(Mistake mistake) async{
+    await _dbHelper.insertMistake(mistake);
+    _loadMistakes(); 
+  }
+
+  Future<void> _loadMistakes() async {
+    List<Mistake> mistakes = await _dbHelper.getMistakes();
+    setState(() {
+      _mistakes = mistakes;
+    });
   }
 }
